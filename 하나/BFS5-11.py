e5380c8a96cf1a21e066bb8e95df145c0b22fb2b
hanana21 <134573117+hanana21@users.noreply.github.com>
2023-09-08 23:01:18 +0900

    이하나/230908

--- 하나/BFS5-11.py
+++ 하나/BFS5-11.py
@@ -1,29 +1,41 @@
+# 출구(NxM)까지 움직여야하는 최소 칸 수: 1(괴물 없는)부분으로 이동해야
 from collections import deque
+# n,m입력받기
+n,m = map(int,input().split())
 
-n,m = map(int, input().split())
-graph = []
+#2차원 리스트 맵 정보 받기
+graph=[]
 for i in range(n):
     graph.append(list(map(int,input())))
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+# 이동할 방향 정의(상하좌우) 
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
 
+# BFS구현 
 def bfs(x,y):
+    # 큐 구현
     queue = deque()
     queue.append((x,y))
-
+    #큐 끝날때까지 반복 
     while queue:
         x,y = queue.popleft()
+        # 4방향 위치 확인 
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-
-            if nx<0 or ny <0 or nx>=n or ny >=m:
+            nx=x+dx[i]
+            ny=y+dy[i]
+            # 미로 공간 벗어나면 무시 
+            if nx<0 or nx>=n or ny<0 or ny>= m:
                 continue
-            if graph[nx][ny] == 0:
+            # 벽 무시 
+            if graph[nx][ny]==0:
                 continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y]+1
+            #처음 방문 하는 경우에만 최단거리 기록 
+            if graph[nx][ny]==1:
+                graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx,ny))
+    # 최단거리 반환
     return graph[n-1][m-1]
-print(bfs(0,0)) 
+# bfs 수행결과 출력 
+print(bfs(0,0))
+
--- 하나/DFS5-10.py
+++ 하나/DFS5-10.py
@@ -1,14 +1,23 @@
+# NxM 얼음틀에 0은 구멍, 1은 칸막이 0끼리 붙을수 있음 => 전체 얼음덩어리 갯수 구하세용 
+
+# N,M 공백으로 구분하여 입력받기 
 n,m = map(int,input().split())
 
-graph =[]
+# 2차원 리스트 맵 정보 입력받기
+graph = []
 for i in range(n):
     graph.append(list(map(int,input())))
 
+# dfs로 특정한 노드를 방문 한 뒤에 연결된 모든 노드들 방문 
 def dfs(x,y):
-    if x <= -1 or x>=n or y <= -1 or y >= m:
+    # 주어진 범위를 벗어나면 종료 
+    if x <= -1 or x >=n or y<= -1 or y >=m :
         return False
+    # 현재 노드 아직 방문 안했다면 
     if graph[x][y] == 0:
+        # 해당 노드 방문 처리 
         graph[x][y] = 1
+        # 상하좌우의 위치도 모두 재귀적으로 호출 
         dfs(x-1,y)
         dfs(x,y-1)
         dfs(x+1,y)
@@ -16,10 +25,12 @@ def dfs(x,y):
         return True
     return False
 
+#모든 노드(위치)에 대하여 음료수 채우기 
 result = 0
 for i in range(n):
     for j in range(m):
+        #현재 위치에서 DFS 수행 
         if dfs(i,j) == True:
-            result += 1
+            result+=1
 
 print(result)
