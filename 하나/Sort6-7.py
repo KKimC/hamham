f1ccaeb5601e5d4383c9992d70065ae925a1339e
hanana21 <134573117+hanana21@users.noreply.github.com>
2023-09-11 22:39:54 +0900

    이하나/230911

---
+++ 하나/Sort6-7.py
@@ -0,0 +1,10 @@
+# 정렬 라이브러리 예제: sorted() 별도로 정렬된 리스트 반환 
+array = [7,5,9,0,3,1,6,2,4,8]
+
+result = sorted(array)
+print(array)
+print(result)
+
+# 정렬 라이브러리 예제: sort 내부원소가 바로 정렬 
+array.sort()
+print(array) 
