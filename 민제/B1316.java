b0b79fe98b8a6b18520173a98b70ca6206274c5c
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-10-21 02:07:24 +0900

    231021 민제(자바)

---
+++ 민제/B1316.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class B1316 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int repeat = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        label:
+        for (int i = 0; i<repeat; i++){
+            List<String> alphabet = new ArrayList<>();
+            String[] input = br.readLine().split("");
+            List<String> list = new ArrayList<>(Arrays.asList(input));
+            alphabet.add(list.get(0));
+            for (int j = 1; j<list.size(); j++){
+                if (list.get(j).equals(list.get(j-1))){
+                    continue;
+                }
+                if (alphabet.contains(list.get(j))){
+                    continue label;
+                }
+                alphabet.add(list.get(j));
+            }
+            answer+=1;
+        }
+        System.out.println(answer);
+    }
+}
