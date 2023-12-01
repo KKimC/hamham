315b3e2170e1d94e04a00965a2423871dfb8be8f
alswp006 <102672547+alswp006@users.noreply.github.com>
2023-12-01 17:25:02 +0900

    231201 민제

---
+++ 민제/B17427.py
@@ -0,0 +1,13 @@
+# 백준 17427번 약수의 합2
+
+# idea
+# N 이하의 자연수들의 약수에서 i들은 g(N)안에 (N // i) * i개만큼 있다.
+
+n = int(input())
+
+answer = 0
+
+for i in range(1, n + 1):
+    answer += (n // i) * i
+
+print(answer)
